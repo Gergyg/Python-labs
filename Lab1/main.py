e995089bed8f77b29da059dd94f45a76dd4c802b
GreggyGg <gad.olg@mail.ru>
2023-02-22 22:50:12 +0300

    Add function of entering a number with verificaton, function of erasing odd elements from list and it's using in main.py

--- Lab1/main.py
+++ Lab1/main.py
@@ -3,17 +3,23 @@ import utilities
 
 print("Hello world")
 
-while True:
-    try:
-        first = int(input("Input first number: "))
-        second = int(input("Input second number: "))
-        break
-    except ValueError:
-        print("Error: invalid number")
+first = utilities.number_entering("Input first number : ")
+second = utilities.number_entering("Input second number : ")
 
-while True:
-    operator = input("Input operator(+, -, *, /): ")
-    result = utilities.Calculate(first, second, operator)
+while True:  # Until operator is correct
+    operator = input("Input operator(+, -, *, /) : ")
+    result = utilities.calculate(first, second, operator)
     print("Result:", result)
-    if result != "Incorrect operator":
+    if result != "Incorrect operator":  # Checking if entered operator is invalid
         break
+
+
+lst = []
+n = utilities.number_entering("Input the number of elements in list : ")
+
+for i in range(0, n):
+    ele = utilities.number_entering("Enter element #" + str(i+1) + " : ")
+    lst.append(ele)
+
+resLst = utilities.get_even_nums_list(lst)
+print("List without odd numbers : ", resLst)
--- Lab1/utilities.py
+++ Lab1/utilities.py
@@ -1,4 +1,15 @@
-def Calculate(num1, num2, op):
+def number_entering(string):
+    while True:  # Until number is entered
+        try:
+            x = int(input(string))
+            break
+        except ValueError:
+            print("Error : invalid number")
+
+    return x
+
+
+def calculate(num1, num2, op):
 
     if op == '+':
         return num1 + num2
@@ -13,3 +24,7 @@ def Calculate(num1, num2, op):
         return num1 / num2 if num2 != 0 else "Error: division by zero"
 
     return "Incorrect operator"
+
+
+def get_even_nums_list(lst):
+    return list(item for item in lst if item % 2 == 0)
